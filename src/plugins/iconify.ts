9a091eb87a8f737766c106ceca128792f00471b3
xd11cc <295900422@qq.com>
2026-05-21 16:31:41 +0800

    优化项目

---
+++ src/plugins/iconify.ts
@@ -0,0 +1,6 @@
+import { addCollection } from '@iconify/vue'
+import epIcons from '@iconify-json/ep/icons.json'
+
+export function installIconify() {
+  addCollection(epIcons)
+}
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,8 +1,10 @@
 import type { App } from 'vue'
 import { installPermissionDirective } from './permission-directive'
 import { installHljs } from './hljs'
+import { installIconify } from './iconify'
 
 export function installPlugins(app: App) {
+  installIconify()
   installPermissionDirective(app)
   installHljs(app)
 }
